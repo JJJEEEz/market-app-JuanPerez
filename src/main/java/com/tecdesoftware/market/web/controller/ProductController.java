d79410014b3f49826aed25cc16118ce18095030c
JJJEEEz <juanestebanperezvelazquez@gmail.com>
2025-07-09 12:29:38 -0600

    Creation of PurchaseRepository

--- src/main/java/com/tecdesoftware/market/web/controller/ProductController.java
+++ src/main/java/com/tecdesoftware/market/web/controller/ProductController.java
@@ -2,41 +2,48 @@ package com.tecdesoftware.market.web.controller;
 
 import com.tecdesoftware.market.domain.Product;
 import com.tecdesoftware.market.domain.service.ProductService;
-import com.tecdesoftware.market.persistence.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-//Le dice a Spring que va a ser el controlador de una API REST
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
+
     // 1. Obtener todos los productos
     @GetMapping
-    public List<Product> getAll() {
-        return productService.getAll();
+    public ResponseEntity<List<Product>> getAll() {
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     // 2. Obtener un producto por ID
     @GetMapping("/{id}")
-    public Optional<Product> getProduct(@PathVariable("id") int productId) {
-        return productService.getProduct(productId);
+    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
+        return productService.getProduct(productId)
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     // 3. Guardar un producto
     @PostMapping
-    public Product save(@RequestBody Product product) {
-        return productService.save(product);
+    public ResponseEntity<Product> save(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
     }
 
     // 4. Eliminar un producto por ID
     @DeleteMapping("/{id}")
-    public boolean delete(@PathVariable("id") int productId) {
-        return productService.delete(productId);
+    public ResponseEntity<Void> delete(@PathVariable("id") int productId) {
+        if (productService.delete(productId)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
